baba6f1aa70943876a2df144e83d5b8bcae1d6bc
Doppelgangers <6666genocide666@mail.ru>
2022-02-23 16:53:52 +0300

    fix venv

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Ozym\Desktop\Bot\venv
+set VIRTUAL_ENV=C:\Users\Ozym\Desktop\TelegramBot\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
